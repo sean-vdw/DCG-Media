edf148c6b71d42761b71524bf6fcfdb7a55a91a8
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-29 12:50:21 -0800

    updated index.js Route

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <Route exact path='/sean-vdw.github.io/DCG-Media/'>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Route>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
