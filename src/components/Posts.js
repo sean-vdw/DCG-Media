bb6a0d42acf7a3a153d73e86d719676efb5646ef
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-01-06 15:43:59 -0800

    added headers file

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,9 @@
 const { parse } = require('rss-to-json');
 
-parse('https://cors-anywhere.herokuapp.com/https://drawingcapital.substack.com/feed')
+parse('https://drawingcapital.substack.com/feed')
   .then(rss => {
-    console.log(JSON.stringify(rss, null, 3));
+    console.log(rss);
   });
-
-
 
 // Posts variable
 const posts = [
